f807fa3b575142bf7c68cf12608a89cc52c5c073
Charles Zhang <aiz.cutei@gmail.com>
2024-06-27 00:32:37 -0500

    first commit

---
+++ src/windows.rs
@@ -0,0 +1,49 @@
+use core::f64::consts::PI;
+use rustfft::{num_complex::Complex64, FftPlanner};
+
+const FS: f64 = 48000.0;
+
+pub fn hann(len: usize) -> Vec<f64> {
+    (0..len)
+        .map(|i| {
+            let x = (i as f64) / (len as f64 - 1.0);
+            0.5 * (1.0 - (2.0 * PI * x).cos())
+        })
+        .collect()
+}
+
+pub fn timederivhann(len: isize) -> Vec<f64> {
+    let m = len / 2;
+    let pos: Vec<f64> = (0..m).map(|x| (x as f64) + 0.5).collect();
+    let neg: Vec<f64> = ((-m)..0).map(|x| (x as f64) + 0.5).collect();
+    let mut framp: Vec<f64> = Vec::with_capacity(len as usize);
+    framp.extend(pos.iter().map(|&x| x / (len as f64)));
+    framp.extend(neg.iter().map(|&x| x / (len as f64)));
+    let fft = FftPlanner::<f64>::new().plan_fft_forward(len as usize);
+    let mut sample: Vec<Complex64> = (0..len as usize)
+        .map(|i| {
+            Complex64::new(
+                0.5 * (1.0 - (2.0 * PI * (i as f64) / (len as f64 - 1.0)).cos()),
+                0.0,
+            )
+        })
+        .collect();
+    fft.process(&mut sample);
+    let mut result: Vec<Complex64> = (0..len as usize)
+        .map(|i| Complex64::new(sample[i].re * framp[i], sample[i].im * framp[i]))
+        .collect();
+    let ifft = FftPlanner::<f64>::new().plan_fft_inverse(len as usize);
+    ifft.process(&mut result);
+    result.iter().map(|x| -x.im * FS / len as f64).collect()
+}
+
+pub fn timeramphann(len: isize) -> Vec<f64> {
+    let m = len / 2;
+    let tramp = (-m..m).map(|x| (x as f64) + 0.5).collect::<Vec<f64>>();
+    (0..len)
+        .map(|i| {
+            let x = (i as f64) / (len as f64 - 1.0);
+            0.5 * (1.0 - (2.0 * PI * x).cos()) * tramp[i as usize] / FS
+        })
+        .collect()
+}
